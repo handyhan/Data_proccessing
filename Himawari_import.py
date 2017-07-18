5cc956a28a62848201d61fad3c220ff36daf8a8b
Hannah N <hannah.nguyen@kcl.ac.uk>
2017-07-18 11:11:28 +0100

    script now correctly cutting for time window

--- Himawari_import.py
+++ Himawari_import.py
@@ -11,7 +11,7 @@ import numpy as np
 file_path = "J:\PhD\earth_observation\Data\Oct_2015\H8_201510_atm.csv"
 
 df = pd.read_csv(file_path)
-fire_conf = df.FIRE_CONFIDENCE
+confidence = df.FIRE_CONFIDENCE
 FRP = df.FRP_0
 latitude = df.LATITUDE
 longitude = df.LONGITUDE
@@ -19,30 +19,40 @@ year=df.year
 month=df.month
 day = df.day
 time=df.time
-time = time.apply(lambda x: '{0:0>6}'.format(x))
-    
-def datetime_pull():
+
+
+
+year=year.astype(int)
+month=month.astype(int)
+day=day.astype(int)
+time=time.astype(int)
+time = time.apply(lambda x: '{0:0>4}'.format(x))
+day = day.apply(lambda x: '{0:0>2}'.format(x))
+time['datetime'] = year.map(str)+month.map(str)+day.map(str)+time.map(str)
+time['datetime'] = pd.to_datetime(time['datetime'],format='%Y%m%d%H%M')
+#print time['datetime']
+
+
+def datetime_pull(): #another way to convert to datetime object
     date_time = []
     for i in range(10000,100000,100):
         times = str(time[i])
         date = datetime.datetime(int(year[i]),int(month[i]),int(day[i]),int(times[0:2]),int(times[2:4]))
         date_time.append(date)
-        
+     
     
     df_datetime = pd.DataFrame({'datetime': date_time})
     
-    data = pd.concat([df_datetime,latitude[0:10],longitude[0:10],FRP[0:10],fire_conf[0:10]], axis=1)
-    return data
+  
 
-data = datetime_pull()
 
-print data.iloc[np.r_[0:10, -10:0]]
-    
-data.index = data['datetime']
+data = pd.concat([latitude,longitude,FRP,confidence], axis=1)
+data = data.set_index(pd.DatetimeIndex(time['datetime']))
+
 start = data.index.searchsorted(datetime.datetime(2015, 10, 14, 0 ,0))
 end = data.index.searchsorted(datetime.datetime(2015, 10, 29,23,59))
 data_in_period = data.ix[start:end]
 
 #data = data.set_index(['datetime'])
 #data_in_period = (data.loc[start_date:end_date])
-print data_in_period.size
+print data_in_period
