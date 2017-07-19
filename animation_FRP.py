92e5d236b7ff5511f7bb40f9f23511687f86b71c
Hannah N <hannah.nguyen@kcl.ac.uk>
2017-07-19 17:02:52 +0100

    done

--- animation_FRP.py
+++ animation_FRP.py
@@ -62,3 +62,74 @@ plt.figure()
 plot_map(data['LONGITUDE'].values, data['LATITUDE'].values, data['FRP_0'].values)
 #plt.savefig('C:\Users\Hannah.N\Documents\Data\test.png')
 plt.show()
+
+
+
+"""
+
+def init():
+    point.set_data([], [], [])
+    return point,
+
+# animation function.  This is called sequentially
+def animate(i):
+    lons, lats, FPRs =  (data['LONGITUDE'].values, data['LATITUDE'].values, data['FRP_0'].values)
+    
+    for lon, lat, FRP in zip(lons, lats, FRPs):
+        
+        x, y = m(lons, lats)
+        #print lons.shape, FRP.shape
+        m.scatter(x, y, c=FRPs, s = 300, marker ='^', zorder=10, alpha = 0.4, norm=mpl.colors.SymLogNorm(linthresh=10, vmin=0, vmax=(500)), cmap=plt.get_cmap('hot_r') )#norm=mpl.colors.SymLogNorm(linthresh=10, vmin=0, vmax=50)) #c=np.abs(FRP),
+        cb = m.colorbar()
+        cb.set_ticks([0,10,100,500,1000])
+    x, y = my_map(lons, lats)
+    point.set_data(x, y)
+    return point,
+    
+
+anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
+                               frames=20, interval=500, blit=True)
+    
+
+
+
+
+
+class AnimatedMap(object):
+    def __init__(self, data_frames):
+        self.dfs = data_frames
+        self.event_map = Basemap(projection=projection, llcrnrlat=llcrnrlat, urcrnrlat=urcrnrlat, llcrnrlon=llcrnrlon, urcrnrlon=urcrnrlon, resolution=resolution,lon_0=lon_0,lat_0=lat_0)
+     
+        self.ani = animation.FuncAnimation(self.fig, self.update, frames=FRAMES, interval=500, 
+                                           init_func=self.setup_plot, blit=True,
+                                           repeat=False)
+
+    def setup_plot(self):
+        self.event_map.drawcoastlines() 
+        self.event_map.drawparallels(np.arange(-40,61.,0.5), labels = [1,0,0,0])
+        self.event_map.drawmeridians(np.arange(100.,140.,0.5), labels = [0,0,1,0])
+        self.event_map.fillcontinents(lake_color='aqua', zorder = 0)
+        self.scat = self.event_map.scatter(x = [], y=[], c=[] , s = 300, marker ='^', zorder=10, alpha = 0.4, norm=mpl.colors.SymLogNorm(linthresh=10, vmin=0, vmax=(500)), cmap=plt.get_cmap('hot_r') )
+        self.scat.colorbar()
+        self.scat.colorbar().set_ticks([0,10,100,500,1000])
+        return self.scat
+
+        
+    
+    
+    def project_lat_lons(self, i):
+        df = data_frames[i]
+        x, y,  = self.event_map(df.lon.values, df.lat.values)
+        x_y = pd.DataFrame({'x': x, 'y': y}, index=df.index)
+        df = df.join(x_y)
+        return df
+
+    def update(self, i):
+        df = self.project_lat_lons(i)
+        new_offsets = np.vstack([df.x.values, df.y.values]).T
+        self.scat.set_offsets(new_offsets)
+        return self.scat,
+
+"""
+#s = AnimatedMap(data)
+#s.ani
